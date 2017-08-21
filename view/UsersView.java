db9b2f46cf301e4dacdc65be28710469d0d5d049
Kharybin <ai.kharybin@gmail.com>
2017-08-21 20:37:25 +0300

    1
    Started upload

---
+++ view/UsersView.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+import java.util.List;
+
+public class UsersView implements View {
+    private Controller controller;
+
+
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (User x: modelData.getUsers()) {
+            System.out.print("\t");
+            System.out.println(x);
+        }
+        System.out.println("===================================================");
+    }
+
+    public void fireEventShowAllUsers(){
+        controller.onShowAllUsers();
+    }
+
+    @Override
+    public void setController(Controller controller) {
+            this.controller = controller;
+    }
+}
